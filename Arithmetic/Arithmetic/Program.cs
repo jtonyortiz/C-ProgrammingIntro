63d2c2e3ad1a5b3058823c87845312e78f5b1f6d
James Ortiz <jamesaortiz75@gmail.com>
2020-07-22 23:02:14 -0400

    Basic Arithmetic Tutorial in C#

---
+++ Arithmetic/Arithmetic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Arithmetic";
+            int a = 8;
+            int b = 4;
+
+            Console.WriteLine("Addition:\t" + (a + b));
+            Console.WriteLine("Subtraction:\t:" + (a - b));
+            Console.WriteLine("Multiplication:\t" + (a * b));
+            Console.WriteLine("Division:\t" + (a / b));
+            Console.WriteLine("Modulus:\t" + (a % b));
+
+            //Postfix Increment Operation:
+            Console.WriteLine("\nPostfix:\t:" + (a++));
+            Console.WriteLine("Postfix Result....:\t" + a);
+
+            //Prefix Increment Operation:
+            Console.WriteLine("\nPrefix:\t:" + (++b));
+            Console.WriteLine("Prefix Result....:\t:" + b);
+            Console.ReadKey();
+
+
+        }
+    }
+}
