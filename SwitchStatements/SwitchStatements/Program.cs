e9f4d008ca88fd83d10169fdb6c2a83f03afee45
James Ortiz <jamesaortiz75@gmail.com>
2020-07-23 01:53:00 -0400

    Switch Statements in C#

---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+            string day;
+            switch (num)
+            {
+                case 1: day = "Monday"; break;
+                case 2: day = "Tuesday"; break;
+                case 3: day = "Wednesday"; break;
+                case 4: day = "Thursday"; break;
+                case 5: day = "Friday"; break;
+                default: day = "Weekend Day"; break;
+            }
+
+            Console.WriteLine("Day " + num + ": " + day);
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
