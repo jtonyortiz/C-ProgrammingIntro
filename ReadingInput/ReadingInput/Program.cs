6683444979f8f66e724badab939acd8ad6817257
James Ortiz <jamesaortiz75@gmail.com>
2020-07-22 22:27:11 -0400

    Basic Input/Output Demo in C#

---
+++ ReadingInput/ReadingInput/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ReadingInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Input";
+            Console.Write("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + name + "!");
+
+            Console.Write("Please enter a number: ");
+            string number = Console.ReadLine();
+
+            //Convert to Integer:
+            int num = Convert.ToInt32(number);
+
+            Console.Write("The number that is inputted was: " + num);
+            Console.ReadKey();
+        }
+    }
+}
