b1451a6156e81a5638168731168d55366e07739c
James Ortiz <jamesaortiz75@gmail.com>
2020-07-22 22:16:08 -0400

    An Intro to C# Development

---
+++ GettingStarted/GettingStarted/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Any Key To Continue...");
+
+            char letter = 'A';
+            int number = 100;
+            float body = 98.6f;
+            double pi = 3.14159;
+            decimal sum = 1000.00m;
+            bool flag = false;
+            string next = "C# Is Fun!";
+
+            Console.WriteLine("char letter\t" + letter);
+            Console.WriteLine("int number\t" + number);
+            Console.WriteLine("float body\t" + body);
+            Console.WriteLine("double pi\t" + pi);
+            Console.WriteLine("decimal sum\t" + sum);
+            Console.WriteLine("bool flag\t" + flag);
+            Console.WriteLine("string text\t" + next);
+
+
+
+
+        }
+    }
+}
