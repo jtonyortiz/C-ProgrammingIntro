688713ae49a5a0a8f4769e6ff2eb5577c8661acd
James Ortiz <jamesaortiz75@gmail.com>
2020-07-23 02:00:15 -0400

    Creating Basic Functions in C#

---
+++ Method/Method/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Method";
+
+            bodyTempC();
+            Console.WriteLine("Fahrenheit:\t" + bodyTempF() + "F");
+            Console.WriteLine("Kelvin:\t" + bodyTempK() + "K");
+            Console.ReadKey();
+
+
+        }
+
+        static void bodyTempC()
+        {
+            Console.WriteLine("Body Temperature..");
+            Console.WriteLine("Centigrade:\t37C");
+        }
+
+        static double bodyTempF()
+        {
+            double temperature = 98.6;
+            return temperature;
+        }
+
+        static int bodyTempK()
+        {
+            int temperature = 310;
+            return temperature;
+        }
+    }
+}
