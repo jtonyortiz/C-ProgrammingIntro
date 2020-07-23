2fb260ee19413d0deed38bb71802333fc51b500f
James Ortiz <jamesaortiz75@gmail.com>
2020-07-23 01:59:35 -0400

    Dictionary (Key-Value) Demo in C#

---
+++ KeyValue/KeyValue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "KeyValue";
+
+            Dictionary<string, string> BookList =
+                new Dictionary<string, string>();
+            BookList.Add("Stuart Yarnold", "Arduino");
+            BookList.Add("Nick Vandome", "Windows 10");
+            BookList.Add("Mike McGrath", "Java");
+
+            Console.WriteLine("Popular Titles...");
+
+            foreach(KeyValuePair<string, string> book in BookList)
+            {
+                Console.WriteLine("Key: " + book.Key +
+                    "\tValue: " + book.Value + " in easy steps");
+            }
+            Console.ReadKey();
+        }
+    }
+}
