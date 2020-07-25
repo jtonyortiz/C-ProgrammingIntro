3d30fb3f33e593fa82fb9b04fe63f7049baaab8e
James Ortiz <jamesaortiz75@gmail.com>
2020-07-25 01:18:54 -0400

    IO Maniplation with String Class in C#

---
+++ ImputAndOutputManipulation/ImputAndOutputManipulation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ImputAndOutputManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Input and Output Manipulation";
+
+            string path = "C:\\Users\\james\\OneDrive\\Desktop\\TopFive.csv";
+
+            try
+            {
+                //
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while((line = reader.ReadLine()) != null)
+                    {
+                        //TODO: Steps 6 - 7 
+                        if (line.IndexOf("Rank") != -1) line = line.ToUpper();
+                        if (line.IndexOf("Sia") != -1) line += " ft.Sean Paul";
+
+                        string[] sub = line.Split(',');
+                        line = String.Format("{0, -30}{1,-20}", sub[1], sub[2]);
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception error)
+            { 
+                Console.WriteLine(error.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ImputAndOutputManipulation/ImputAndOutputManipulation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImputAndOutputManipulation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImputAndOutputManipulation")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4da148c5-3abf-4add-90ff-fdcdaf3b8c96")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
