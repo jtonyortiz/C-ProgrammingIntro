6f5c0a3ce7ae0343d353ac0ea313235b67712c02
James Ortiz <jamesaortiz75@gmail.com>
2020-07-26 06:37:36 -0400

    Calling Base Constructors in C#

---
+++ CallingBaseConstructors/CallingBaseConstructors/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingBaseConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Calling Base Constructors";
+
+
+            //Create instances of the derived classes:
+            Daughter anna = new Daughter();
+            Son brad = new Son();
+            Son carl = new Son(100);
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("Parent Called");
+        }
+
+        //Overloaded constructor:
+        public Parent(int num)
+        {
+            Console.WriteLine("Parent+ Called: " + num);
+        }
+
+    }
+
+    //Daughter inherits parent:
+    public class Daughter: Parent
+    {
+        public Daughter()
+        {
+            Console.WriteLine("\tDaughter Called\n");
+        }
+    }
+
+    //Son inherits parent:
+    public class Son: Parent
+    {
+        public Son()
+        {
+            Console.WriteLine("\tSon Called\n");
+        }
+
+        //Calling an overloaded constructor of the base class:
+        public Son(int num): base(num)
+        {
+            Console.WriteLine("\tSon+ Called: " + num);
+        }
+    }
+
+}
