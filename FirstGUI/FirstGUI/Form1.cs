5488b49be6c0052c22752cf37d021876b20c0fb6
James Ortiz <jamesaortiz75@gmail.com>
2020-07-29 01:44:24 -0400

    First example of a GUI in C#

---
+++ FirstGUI/FirstGUI/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Fuchsia;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Lime;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Aqua;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C# Programming in easy steps");
+        }
+    }
+}
