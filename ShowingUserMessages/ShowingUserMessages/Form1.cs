019a0619a2ef5948c5d5f4811274db8ebe2215bf
James Ortiz <jamesaortiz75@gmail.com>
2020-07-29 01:47:20 -0400

    Showing Errors & User Msgs. in C# (GUI)

---
+++ ShowingUserMessages/ShowingUserMessages/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowingUserMessages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch(NotImplementedException notImp)
+            {
+                //Steps: 4 - 5:
+
+
+                DialogResult result =
+                    MessageBox.Show("Proceed?", notImp.Message,
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+
+                textBox1.Text = result.ToString();
+                if (result == DialogResult.Yes) label1.Text = "Proceeding...";
+                if (result == DialogResult.No) label1.Text = "Stopping...";
+
+
+            }
+        }
+    }
+}
