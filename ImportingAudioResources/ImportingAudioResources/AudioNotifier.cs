67bfa11ce8cf9642048387e7ed9b2fd3a81bf49b
James Ortiz <jamesaortiz75@gmail.com>
2020-07-29 13:46:39 -0400

    Event Handling for Audio

---
+++ ImportingAudioResources/ImportingAudioResources/AudioNotifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace ImportingAudioResources
+{
+    public partial class AudioNotifier : Form
+    {
+        public AudioNotifier()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SoundPlayer notifier =
+                new SoundPlayer(Properties.Resources.notify);
+            //Notfying user GUI:
+            label1.Text = "Notifying...";
+            //Play music:
+            notifier.Play();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- ImportingAudioResources/ImportingAudioResources/Program.cs
+++ ImportingAudioResources/ImportingAudioResources/Program.cs
@@ -16,7 +16,7 @@ namespace ImportingAudioResources
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AudioNotifier());
         }
     }
 }
