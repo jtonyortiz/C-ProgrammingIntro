8458d99f1ca29872882486a88c0af6de23abe4ca
James Ortiz <jamesaortiz75@gmail.com>
2020-07-29 01:45:45 -0400

    Creating Partial Classes in C#

---
+++ PartIalClasses/PartIalClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartIalClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an instance of the Sailboat class:
+            Sailboat boat = new Sailboat("Laser", "Classic");
+            boat.describe();
+            Console.ReadKey();
+        }
+    }
+
+    //Part of the same namespace, hence the term "partial classes":
+    public partial class Sailboat
+    {
+        public void describe()
+        {
+            Console.WriteLine("Sailboat: {0} {1}", make, model);
+        }
+    }
+}
---
+++ PartIalClasses/PartIalClasses/Sailboat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartIalClasses
+{
+    public partial class Sailboat
+    {
+        public string make;
+        public string model;
+
+        //Class Constructor:
+        public Sailboat(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+        }
+
+    }
+}
