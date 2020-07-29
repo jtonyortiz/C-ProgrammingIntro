85085e20f3d510197bc8510e3c00534ba3326cd4
James Ortiz <jamesaortiz75@gmail.com>
2020-07-29 01:46:25 -0400

    Providing Capability Classes in C#

---
+++ ProvidingCapabilityClasses/ProvidingCapabilityClasses/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvidingCapabilityClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Capability Classes in OOP";
+            Pigeon joey = new Pigeon();
+            Chicken lola = new Chicken();
+            Caller call = new Caller();
+            call.describe(joey);
+            call.describe(lola);
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Bird
+    {
+        public abstract void talk();
+
+        public abstract void fly();
+    }
+
+    public sealed class Pigeon: Bird
+    {
+        public override void talk()
+        {
+            Console.WriteLine("Pigeon Says: Coo Coo!");
+
+        }
+
+        public override void fly()
+        {
+            Console.WriteLine("A Pigeon Flies Away...");
+        }
+
+    }
+
+    public sealed class Chicken: Bird
+    {
+        public override void talk()
+        {
+            Console.WriteLine("Chicken Says: Cluck! Cluck!");
+        }
+
+        public override void fly()
+        {
+            Console.WriteLine("I'm A chicken - I Can't Fly");
+        }
+    }
+
+    public sealed class Caller
+    {
+        public void describe(Bird obj)
+        {
+            obj.talk();
+            obj.fly();
+        }
+    }
+}
