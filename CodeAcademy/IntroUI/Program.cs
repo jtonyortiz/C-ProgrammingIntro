178255c26567d0cef8ff7f9e2076927b04171942
James Ortiz <jamesaortiz75@gmail.com>
2021-04-04 23:37:06 -0400

    UI Creation in C#

---
+++ CodeAcademy/IntroUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel{
+                FirstName = "James",
+                LastName = "Ortiz"
+            };
+
+            System.Console.WriteLine($"{p.FirstName} {p.LastName} is my name.");
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("This is a test");
+            System.Console.WriteLine("The end.");
+        }
+    }
+}
