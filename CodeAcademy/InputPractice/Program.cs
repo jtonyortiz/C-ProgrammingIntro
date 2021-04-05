7218d73f57b1764a49f049e8d340dee1807fa978
James Ortiz <jamesaortiz75@gmail.com>
2021-04-04 23:35:52 -0400

    Input and Output Practice on C#

---
+++ CodeAcademy/InputPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InputPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old are you?");
+            //Reads input from user:
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+        }
+    }
+}
