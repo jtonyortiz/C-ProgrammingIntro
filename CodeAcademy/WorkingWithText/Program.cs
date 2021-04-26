128c43de561d046ab62744cb434e3d756f77cebe
James Ortiz <jamesaortiz75@gmail.com>
2021-04-25 23:23:37 -0400

    Sample Test Program in C#

---
+++ CodeAcademy/WorkingWithText/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* use this space to write your own short program! 
+               Here's what you learned:
+
+                DATA TYPES: char, string
+                STRING INTERPOLATION: $"blah blah"
+                STRING INFO: .Length, .IndexOf()
+                PARTS OF STRINGS: bracketNotation[], .Substring() 
+                 STRING MANIPULATION: .ToUpper(), .ToLower()
+
+                 Good luck! */
+
+                    string name_;
+                    Console.Write("Please enter name of character: ");
+                    name_ = Console.ReadLine();
+
+                    string topic_;
+                    Console.WriteLine("Please enter topic of the story: ");
+                    topic_ = Console.ReadLine();
+
+                    string storyLine = $"This is a story about {name_}, and it is short but {topic_} packed.";
+
+                    Console.WriteLine(storyLine);
+        }
+    }
+}
